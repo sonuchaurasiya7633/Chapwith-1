b0013a5369ba8753818fedc63c7efe1462ef5708
Sonu Kumar <sonukumar763303@gmail.com>
2025-07-13 15:54:17 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://chapwith-1-bec.onrender.com",
         changeOrigin: true,
       },
     },
